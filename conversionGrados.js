c38a7ca6ade82f669d7496e0fb537043dc912534
Aamira111 <adrimira91@gmail.com>
2023-05-26 22:14:03 -0300

    Agrego el archivo conversionesGrados.js

---
+++ conversionGrados.js
@@ -0,0 +1,13 @@
+//¿Cuántos grados C son 50 F?
+
+let farenheit = 50;
+let  celcius= (farenheit - 32) * 5 / 9;
+
+console.log(celcius);
+
+
+//¿Cuántos grados C son 75 F?
+let farenheit1 = 75;
+let  celcius1= (farenheit1 - 32) * 5 / 9;
+
+console.log(celcius1);      
