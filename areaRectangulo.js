477cafb72ba7f6e571a12c049ac4bbf3f1d4543e
Aamira111 <adrimira91@gmail.com>
2023-05-26 22:15:50 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,17 @@
+//¿Cuál es el área de un rectángulo de base 5cm y altura 3cm?
+
+let base = 5;
+let altura = 3;
+let area = (base * altura)
+
+console.log(area);
+
+
+
+//¿Cuál es el área de un rectángulo de base 12.5cm y altura 5.3cm?
+
+let base1 = 12.5;
+let altura1 = 5.3;
+let area1 = (base1 * altura1)
+
+console.log(area1);
---
+++ promedio.js
@@ -0,0 +1,25 @@
+//¿Cuál es el promedio entre 5, 7 y 9?
+let prom1 = 5;
+let prom2 = 7;
+let prom3 = 9;
+
+let promedio1 = (prom1 + prom2+ prom3) / 3
+console.log(promedio1);
+
+
+//¿Cuál es el promedio entre -2 , -3, -8?
+let num1 = -2;
+let num2 = -3;
+let num3 = -8;
+
+let promedio2= (num1 + num2+ num3) / 3
+console.log(promedio2);
+
+
+//¿Cuál es el promedio entre -1.2, 5.3, -3.3?
+let nume1 = -1.2;
+let nume2 = 5.3;
+let nume3 = -3.3;
+
+let promedio3 = (nume1 + nume2+ nume3) / 3
+console.log(promedio3);
