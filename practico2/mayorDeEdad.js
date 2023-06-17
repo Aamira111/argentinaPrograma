5bd090da60e6cdd2b740797bc5a417f0894ebb4c
Aamira111 <adrimira91@gmail.com>
2023-06-17 16:33:17 -0300

    ejercicio1-a

---
+++ practico2/mayorDeEdad.js
@@ -0,0 +1,14 @@
+/*Solicite al usuario ingresar su edad. Si la edad es mayor o igual a 18,
+muestra por consola el mensaje: "Eres mayor de edad". De lo contrario,
+muestra por consola el mensaje: "Eres menor de edad". Guardar en un
+archivo llamado mayorDeEdad.js*/
+
+
+let edad = prompt ("ingrese su edad");
+
+if (edad >= 18){
+    alert("Eres mayor de edad");
+}else{
+    alert("Eres menor de edad");
+};
+
